09e1ef2a88b75330bbbc5d15d736f035c9f15914
GabrielaMGC <gmgc2010@gmail.com>
2023-04-26 16:28:54 -0300

    criando os motores da ponte h

--- carrinhobluetoothg.ino
+++ carrinhobluetoothg.ino
@@ -1,3 +1,17 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial bt(0, 1);
+
+//LADO ESQUERDO
+int ENA = 10;
+int IN1 = 9;
+int IN2 = 8;
+
+//LADO DIREITO
+int ENB = 5;
+int IN3 = 7;
+int IN4 = 6;
+
 void setup() {
   // put your setup code here, to run once:
 
